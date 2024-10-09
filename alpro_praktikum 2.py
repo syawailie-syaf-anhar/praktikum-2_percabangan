55678545357182dd483c6b604e1e64b7e6611604
syawailie-syaf-anhar <syawailie0611@gmail.com>
2024-10-09 10:31:41 +0700

    alpro_praktikum 2.py

--- alpro_praktikum 2.py
+++ alpro_praktikum 2.py
@@ -1,35 +1,40 @@
-number = int(input("Enter a number:"))
+#Paraktikum 2 Alpro 
+
+#menentukan apakah sebuah angka itu ganjl atau genap
+number = int(input("input a number: "))
 if(number%2==0):
-    print("number is even")
+    print(f"number {number} is even")
 else:
     print("number is Odd")
-print("\n===========")
+print("\n==========================")
 
-score = int(input("Enter your test score (0-100) : "))
+#menentukan rance nilai 
+score = int(input("imput your test score (0-100) : "))
 
 if 90 <= score <= 100 :
-    print("Very good")
+    print("your score is very good")
 elif  80 <= score <= 89:
-    print("Good")
+    print("your score is good")
 elif 70 <= score <= 79 :
-    print("Fair")
+    print("your score is Fair")
 elif 60 <= score <= 69:
-    print("poor")
+    print("your score is poor")
 else:
     score < 60
-    print("very poor")
-print("\n==========")
+    print("your score is very poor")
+print("\n========================")
 
-age = int(input("Enter your age : "))
-blood = int(input("Enter your blood pressure : "))
+#menentukan status kesehatan berdasarkan umur dan tekanan darah
+age = int(input("input your age : "))
+blood = int(input("input your blood pressure : "))
 
 if age >= 60 and blood > 140 :
-    print("High")
+    print("Health Status is High")
 elif age >= 60 and blood <= 140 :
-    print("Normal")
+    print("Health Status is Normal")
 elif age < 60 and blood > 130 :
-    print("High")
+    print("Health Status is High")
 elif age < 60 and blood <= 130 :
-    print("Normal")
+    print("Health Status is Normal")
 else:
     print("Not Defined")
